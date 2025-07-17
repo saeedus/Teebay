368348d1791907dda97f33adf91de07a300092d9
Saeedus Salehin <saeedussalehin22@gmail.com>
2025-07-18 01:21:08 +0600

    Redirect to AuthActivity from MainActivity

--- app/src/main/java/com/sazim/teebay/MainActivity.kt
+++ app/src/main/java/com/sazim/teebay/MainActivity.kt
@@ -1,38 +1,21 @@
 package com.sazim.teebay
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import com.sazim.teebay.core.presentation.ui.theme.TeebayTheme
+import com.sazim.teebay.auth.presentation.AuthActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            TeebayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+        val isLoggedIn = false
+
+        if (isLoggedIn) {
+            //TODO: Navigate to HomeActivity or Dashboard
+        } else {
+            startActivity(Intent(this, AuthActivity::class.java))
+        }
+        finish()
+    }
 }
