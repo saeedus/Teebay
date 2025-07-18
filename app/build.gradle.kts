83cb5ac417beb1c2c7bedf0e2c50965275c6ce86
Saeedus Salehin <saeedussalehin22@gmail.com>
2025-07-19 01:26:45 +0600

    This commit introduces a new "real" product flavor to allow network requests to a physical device IP address (192.168.0.101)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,10 @@ android {
             dimension = "environment"
             buildConfigField("String", "BASE_URL", "\"http://10.0.2.2:8000/api/\"")
         }
+        create("real") {
+            dimension = "environment"
+            buildConfigField("String", "BASE_URL", "\"http://192.168.0.101:8000/api/\"")
+        }
     }
 }
 
